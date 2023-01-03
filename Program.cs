1ea6986bc05a4d8fff7153f2008c15545eb6f58b
George Maia <georgemaiaf@gmail.com>
2023-01-03 12:33:44 -0300

    feat - Smartphone, Nokia, Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("5588981402020", "C20", "asd123", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("5588958694758", "13 pro", "qwe456", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
